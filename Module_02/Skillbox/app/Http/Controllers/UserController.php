e96228fac6a3e346a900b8843b73f8dbe93f893b
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-08-21 15:40:45 +0500

    finished module_11 tasks

--- Module_02/Skillbox/app/Http/Controllers/UserController.php
+++ Module_02/Skillbox/app/Http/Controllers/UserController.php
@@ -13,6 +13,7 @@ class UserController extends Controller
 {
     public function index(): View
     {
+        $this->authorize('view-any', User::class);
         $users = User::all();
         return view('getUserData', compact('users'));
     }
--- Module_02/Skillbox/app/Models/User.php
+++ Module_02/Skillbox/app/Models/User.php
@@ -14,11 +14,36 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string, string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
-        'surname',
         'email',
+        'password',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function isAdmin(): bool
+    {
+        return $this->is_admin == 1;
+    }
 }
